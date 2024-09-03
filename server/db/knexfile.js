021ed2d0231f852e26a553bcb8de66f2efd31b37
Amy <58102593+achoo-o@users.noreply.github.com>
2024-09-03 14:50:02 +1200

    Update knexfile.js

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -14,39 +14,20 @@ if (process.env.NODE_ENV === 'production') {
 export default {
   development: {
     client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: Path.join(__dirname, 'dev.sqlite3'),
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
-  },
-
-  test: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: ':memory:',
-    },
-    migrations: {
-      directory: Path.join(__dirname, 'migrations'),
-    },
-    seeds: {
-      directory: Path.join(__dirname, 'seeds'),
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
+@@ -34,13 +41,14 @@ export default {
   },
 
   production: {
+    client: 'sqlite3',
     client: 'pg',
     useNullAsDefault: true,
     connection: {
+      filename: '/app/storage/prod.sqlite3',
       connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
     },
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
     migrations: {
       directory: Path.join(__dirname, 'migrations'),
     },
