bc2b585b7d65197d6490dc5df60ece2b7ea70e15
Amy <58102593+achoo-o@users.noreply.github.com>
2024-09-03 14:42:19 +1200

    set up railway db

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,8 +1,15 @@
 import * as Path from 'node:path'
 import * as URL from 'node:url'
+import dotenv from 'dotenv'
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config()
+} else {
+  dotenv.config({ path: Path.join(__dirname, '../../.env') })
+}
 
 export default {
   development: {
@@ -34,13 +41,14 @@ export default {
   },
 
   production: {
-    client: 'sqlite3',
+    client: 'pg',
     useNullAsDefault: true,
     connection: {
-      filename: '/app/storage/prod.sqlite3',
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+    migrations: {
+      directory: Path.join(__dirname, 'migrations'),
     },
   },
 }
